eeb7186eb83d97c443f8adc36ff0c9b92ff984f3
leia0 <leia840419@gmail.com>
2024-07-07 14:05:02 +0800

    0710

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
 import gradio as gr
+from gradio_calendar import Calendar
+import datetime
 
-def greet(name):
-    return "Hello " + name + "!!"
+def is_weekday(date: datetime.datetime):
+    weekday = str(date.weekday()) 
+    return date.weekday() 
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+with gr.Blocks() as demo:
+    gr.Markdown(
+    """
+    # VICTOR's 心情章魚
+    Start typing below to see the output.
+    心情指數 0(差)~10(好)
+    """)
+    with gr.Row():
+        num1 = gr.Slider(1, 10)
+    output = gr.Image("1.png")
+
+
+
+    @gr.on(inputs=[num1], outputs=output)
+    def sum(num1):
+        if num1>=1 and num1<=5:
+            b="2.png"
+        else :
+            b="1.png"
+        return b
+
+ 
+
+
+demo.launch(share=True) 
